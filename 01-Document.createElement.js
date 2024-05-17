69e9a5918feed1c14213af36b21f6842c00367be
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-05-17 08:38:03 +0530

    Add files via upload

---
+++ 01-Document.createElement.js
@@ -0,0 +1,6 @@
+// let newElement = document.createElement('div');
+let newElement = document.createElement('input');
+
+document.body.appendChild(newElement);
+// console.dir(newElement);
+// console.log(newElement);
