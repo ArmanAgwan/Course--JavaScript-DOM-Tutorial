8ff96aed3d6065294635b49606fbadd11a7c39b7
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-05-20 16:05:06 +0530

    Add files via upload

---
+++ 03-Text_Content.js
@@ -0,0 +1,7 @@
+let textcontent = document.createElement('h1');
+textcontent.setAttribute('id','title');
+textcontent.innerHTML='Welcome to Code'
+
+console.log(textcontent);
+
+textcontent.textContent = 'Mango and Bananas';
