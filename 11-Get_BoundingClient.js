4c76c10073b434773866f6abaed23e1375cdaaf6
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-05-27 16:59:16 +0530

    Add files via upload

--- 11-Get_BoundingClient.js
+++ 11-Get_BoundingClient.js
@@ -1,4 +1,14 @@
 const box = document.querySelector("div.box");
-const rect = box.getBoundingClientRect();
+// const rect = box.getBoundingClientRect();
 
-console.log(rect);
+// console.log(rect);
+
+
+window.addEventListener("scroll", function() {
+    // console.log(window.innerHeight);
+    // console.log(box.getBoundingClientRect().top);
+
+    if (box.getBoundingClientRect().top < window.innerHeight) {
+        box.style.background = 'red';
+    }
+});
