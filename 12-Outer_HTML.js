9f7b62f3807c03318a8897fb46335467e931b32b
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-05-27 17:16:15 +0530

    Add files via upload

---
+++ 12-Outer_HTML.js
@@ -0,0 +1,7 @@
+const h2Title = document.getElementById("title");
+
+//Get the outerHTML
+console.log(h2Title.outerHTML);
+
+// Change the outerHTML
+h2Title.outerHTML = "<div id='dom'>I've been changed! Element.outerHTML - JS DOM - dcode</div>";
