1861160a1bb4adf6d995da7de64087549a52cebf
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-05-29 08:34:12 +0530

    Add files via upload

---
+++ 14-Query_SelectorAll.js
@@ -0,0 +1,5 @@
+const list = document.querySelectorAll('li');
+console.log(list); // [li, li, li, li, li, li]
+
+const list2 = document.querySelectorAll('#list2 li');
+console.log(list2); // [li, li, li]
