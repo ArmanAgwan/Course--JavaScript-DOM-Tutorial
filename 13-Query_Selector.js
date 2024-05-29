05cb1b5664efee23bca44b78afa1aaa9daf6d2d5
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-05-29 08:28:31 +0530

    Add files via upload

--- 13-Query_Selector.js
+++ 13-Query_Selector.js
@@ -1,3 +1,6 @@
 // Query Selector lets you find the first Element that matches ths CSS selector
-const pageTitle = document.querySelector("h2");
-console.log(pageTitle);
+// const result= document.querySelector("#title");
+const container2 = document.querySelector("#container2");
+const result= container2.querySelector(".box");
+// const result= document.querySelector(".box");
+console.log(result);
